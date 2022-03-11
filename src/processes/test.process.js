678f8ab8cf97a5c15936a1873802db2803b1cb5c
Joe Puzzo <jpuzzo@tesla.com>
2022-03-11 06:41:05 -0800

    added fail for two attempts

--- src/processes/test.process.js
+++ src/processes/test.process.js
@@ -1,4 +1,8 @@
 const testProcess = async (job) => {
+   console.log('ATTEMPTS', job.attemptsMade);
+   if(job.attemptsMade < 2 ){
+      throw new Error('Server is down');
+   }
    console.log('Test process', job.data);
    return 'Wooo!!!';
 };
